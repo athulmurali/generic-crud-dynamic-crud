9174f74caa09296baef1879836e0c485403d011d
Athul Muralidharan <athulmurali@gmail.com>
2018-12-06 21:50:04 -0500

    Checking string return

--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,44 @@
 const express = require('express');
 const router = express.Router();
 
+const TABLE_NAME = "tableName"
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.send('Hola! I  am a cool api');
 });
 
 //
-router.get('/:path',(req,res,next)=>{
-    console.log(req.params)
+router.get('/:'+TABLE_NAME,(req,res,next)=>{
 
-    connso:
-    res.send('api path is :' + JSON.stringify(req.params));
+
+
+    res.send('Checking if exists  :  tableName : ' + path);
+
 })
 
+
+router.post('/:'+TABLE_NAME,(req,res,next)=>{
+
+    const path =req.params[TABLE_NAME]
+
+    const schem =  [1,2,4]
+
+    schem.map(val =>{
+        console.log(val)
+        return {[val] : String}
+    })
+    console.log(schem)
+
+    res.send('Checking if exists  :  tableName : ' + path);
+})
+
+
+router.put('/:'+TABLE_NAME,(req,res,next)=>{
+
+    const path =req.params[TABLE_NAME]
+    res.send('Checking if exists  :  tableName : ' + path);
+})
 
 
 module.exports = router;
